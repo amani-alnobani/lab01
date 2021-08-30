062f3bef1582122d3250c4ad43877b0ae934b017
amani-alnobani <amanialnobani@gmail.com>
2021-08-30 16:31:56 +0300

    finish

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import Main from './components/Main';
+
+import Header from './components/Header';
+
+import Footer from './components/Footer';
+
+
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+        <Header/>
+        <Main />
+        <Footer />
+
+      </div>
+    );
+  }
+}
+
+
+
+export default App;
