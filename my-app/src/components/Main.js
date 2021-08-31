b4120bb103827f9ed8143b8e08976e50473f80a1
amani-alnobani <amanialnobani@gmail.com>
2021-08-31 09:06:02 +0300

    done

--- my-app/src/components/Main.js
+++ my-app/src/components/Main.js
@@ -31,7 +31,7 @@ class Main extends React.Component {
                 <HornedBeasts
 
                     title='Unicorn Head'
-                    imageUrl="https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg"
+                    imageUrl="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"
                     description="Someone wearing a creepy unicorn head mask"
                     keyword="unicorn"
                     horns={1}
@@ -39,7 +39,7 @@ class Main extends React.Component {
                 <HornedBeasts
 
                     title='Unicorn Head'
-                    imageUrl="https://images.unsplash.com/photo-1518946222227-364f22132616?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=4836a6fca62e7dce9324346bacfde085&auto=format&fit=crop&w=2534&q=80"
+                    imageUrl="https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg"
                     description="Someone wearing a creepy unicorn head mask"
                     keyword="unicorn"
                     horns={1}
@@ -47,7 +47,7 @@ class Main extends React.Component {
                 <HornedBeasts
 
                     title='UniLego'
-                    imageUrl="https://i.pinimg.com/736x/b4/61/06/b46106830b841017ea59870b27ec18dc--narwhals-a-unicorn.jpg"
+                    imageUrl="https://images.unsplash.com/photo-1518946222227-364f22132616?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=4836a6fca62e7dce9324346bacfde085&auto=format&fit=crop&w=2534&q=80"
                     description="Lego figurine dressed in a unicorn outfit"
                     keyword="unilego"
                     horns={1}
