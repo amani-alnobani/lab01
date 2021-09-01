d4670ccfcf156cfba9232c9fb2d250ad21d9bf82
amani-alnobani <amanialnobani@gmail.com>
2021-09-01 14:29:01 +0300

    done

--- my-app/src/components/Main.js
+++ my-app/src/components/Main.js
@@ -1,31 +1,35 @@
 import React from 'react';
 import HornedBeasts from './HornedBeast';
+import Row from 'react-bootstrap/Row';
 
 
 //parent
 
 class Main extends React.Component {
-    
+
     render() {
         // console.log(this.props);
         return (
-            <main> {
-                this.props.hornedBeastData.map((beast) => {
-                    return (
-                        <HornedBeasts
-                            title={beast.title}
-                            imageUrl={beast.image_url}
-                            description={beast.description}
-                            keyword={beast.keyword}
-                            horns={beast.horns}
-                            filterBeastByName={this.props.filterBeastByName}
-                            showRequestMessage={this.showRequestMessage}
-                           
+            <main>
+                <Row xs={1} md={3} className="g-4">
+                    {
+                        this.props.hornedBeastData.map((beast) => {
+                            return (
+                                <HornedBeasts
+                                    title={beast.title}
+                                    imageUrl={beast.image_url}
+                                    description={beast.description}
+                                    keyword={beast.keyword}
+                                    horns={beast.horns}
+                                    filterBeastByName={this.props.filterBeastByName}
+                                    showRequestMessage={this.showRequestMessage}
 
-                        />
-                    )
-                })
-            }
+
+                                />
+                            )
+                        })
+                    }
+                </Row>
             </main >
         );
     }
